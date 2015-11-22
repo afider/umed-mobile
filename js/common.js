a20443b2d4ee9a1599311e7d11379ad41076cea3
Evgeniy Beskrovnyy <e.beskrovnyy@gmail.com>
2015-11-22 21:38:43 +0400

    change region page is done

--- js/common.js
+++ js/common.js
@@ -3,12 +3,23 @@
 
 		document.getElementById('svg-icons').innerHTML = SVG_ICONS;
 		initCustomSelect(); // кастомные селекты
- 		
+		animateRegionSelect(); // анимирование выбора региона
 	});
 
-
-	function initCustomSelect () {
+	function initCustomSelect() {
 		
-        $('.js-select').customSelect();
-	} // initCustomSelect
+		$('.js-select').customSelect();
+	}
 
+	function animateRegionSelect() {
+		var openState = 'sub-header_open';
+		var control = $('.sub-header_select');
+		var showHideTarget = $('.more-items');
+
+		control.on('click', function() {
+			var self = $(this);
+			self.toggleClass( openState );
+			showHideTarget.slideToggle( 300 );
+			
+		});
+	}
